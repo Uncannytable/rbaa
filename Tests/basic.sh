a9152622a236f8fe8cbd47d222126eaff4b21ca3
Uncannytable <vmpaisante@gmail.com>
2015-07-28 16:50:58 -0300

    Fix run scripts to use new PATH variable changes

--- Tests/basic.sh
+++ Tests/basic.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/Ecosoc/llvm-3.5.0/lib/Ecosoc/llvm-sra/SAGE/bin/sage-opt -load Python.so -load SAGE.so -load SRA.so -load RangeBasedAliasAnalysis.so -basicaa -aa-eval $1.redef.bc -o /dev/null
+sage-opt -load Python.so -load SAGE.so -load SRA.so -load RangeBasedAliasAnalysis.so -basicaa -aa-eval $1.redef.bc -o /dev/null
--- Tests/rbaa.sh
+++ Tests/rbaa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-~/Ecosoc/llvm-3.5.0/lib/Ecosoc/llvm-sra/SAGE/bin/sage-opt -load Python.so -load SAGE.so -load SRA.so -load RangeBasedAliasAnalysis.so -range-based-aa -aa-eval $1.ssify.bc -o $1.redef.bc 
-~/Ecosoc/llvm-3.5.0/Release+Asserts/bin/llvm-dis $1.redef.bc -o $1.redef.ll
+sage-opt -load Python.so -load SAGE.so -load SRA.so -load RangeBasedAliasAnalysis.so -range-based-aa -aa-eval $1.ssify.bc -o $1.redef.bc 
+llvm-dis $1.redef.bc -o $1.redef.ll
