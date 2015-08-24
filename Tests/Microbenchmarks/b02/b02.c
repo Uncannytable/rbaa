ed24df8d50ed161cc909398ea012ff1f1f4236b9
Vitor <vmpaisante@gmail.com>
2015-08-24 17:26:47 -0300

    Fixed microbenchmarks

--- Tests/Microbenchmarks/b02/b02.c
+++ Tests/Microbenchmarks/b02/b02.c
@@ -4,12 +4,12 @@ int bench_B(int* v, int N) {
   int sEven = 0;
   int i;
   for (i = 0; i < N; i += 2) {
-    int j = i + 1;
-    sOdd += v[i];
-    sEven += v[j];
+    int* w = v + i;
+    sOdd += *w;
+    sEven += *(w + 1);
   }
   return sOdd > sEven ? 1 : 0;
-} 
+}
 int main(int argc, char** argv)
 {
   int x = malloc(100*sizeof(int));
--- Tests/Microbenchmarks/b05/b05.c
+++ Tests/Microbenchmarks/b05/b05.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 int bench_E(int *v, int a) {
-  int N = a % 2 ? 0 : 1;
-  int M = a % 2 ? 2 : 3;
+  int N = 0;
+  int M = 2;
+  if(a%2){
+    N = 1;
+    M = 3;
+  }
   int t0 = v[N];
   int t1 = v[M];
   return t0 + t1;
